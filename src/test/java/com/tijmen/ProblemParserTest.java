ffbe09c0f798598e4f8f0a31248ec66502fad5de
t.vanderkemp <t.vanderkemp@gouwit.nl>
2019-10-21 18:16:25 +0200

    Add test for parsing second sample file

--- src/test/java/com/tijmen/ProblemParserTest.java
+++ src/test/java/com/tijmen/ProblemParserTest.java
@@ -11,34 +11,63 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ProblemParserTest {
 
-    @Test
-    public void test() {
-        ProblemParser parser = new ProblemParser();
-        InputStream testFile = TestFile.get("sample1.txt");
-        Problem problem = parser.parse(testFile);
+  @Test
+  public void testSample1() {
+    ProblemParser parser = new ProblemParser();
+    InputStream testFile = TestFile.get("sample1.txt");
+    Problem problem = parser.parse(testFile);
 
-        assertThat(problem.getGraph().getVertices()).containsExactly(
-                new Vertex(1),
-                new Vertex(2),
-                new Vertex(3),
-                new Vertex(4),
-                new Vertex(5),
-                new Vertex(6),
-                new Vertex(7));
-        assertThat(problem.getGraph().getEdges()).containsExactly(
-                edge(1, 2),
-                edge(1, 4),
-                edge(2, 3),
-                edge(2, 5),
-                edge(4, 5),
-                edge(5, 6),
-                edge(5, 7),
-                edge(6, 7)
-        );
-        assertThat(problem.getNumberOfBins()).isEqualTo(4);
-    }
+    assertThat(problem.getGraph().getVertices()).containsExactly(
+        new Vertex(1),
+        new Vertex(2),
+        new Vertex(3),
+        new Vertex(4),
+        new Vertex(5),
+        new Vertex(6),
+        new Vertex(7));
+    assertThat(problem.getGraph().getEdges()).containsExactly(
+        edge(1, 2),
+        edge(1, 4),
+        edge(2, 3),
+        edge(2, 5),
+        edge(4, 5),
+        edge(5, 6),
+        edge(5, 7),
+        edge(6, 7)
+    );
+    assertThat(problem.getNumberOfBins()).isEqualTo(4);
+  }
 
-    private Edge edge(int v1, int v2) {
-        return new Edge(new Vertex(v1), new Vertex(v2));
-    }
+  @Test
+  public void testSample2() {
+    ProblemParser parser = new ProblemParser();
+    InputStream testFile = TestFile.get("sample2.txt");
+    Problem problem = parser.parse(testFile);
+
+    assertThat(problem.getGraph().getVertices()).containsExactly(
+        new Vertex(1),
+        new Vertex(2),
+        new Vertex(3),
+        new Vertex(4),
+        new Vertex(5),
+        new Vertex(6),
+        new Vertex(7),
+        new Vertex(8));
+    assertThat(problem.getGraph().getEdges()).containsExactly(
+        edge(1, 2),
+        edge(1, 4),
+        edge(2, 3),
+        edge(2, 5),
+        edge(4, 5),
+        edge(5, 6),
+        edge(5, 7),
+        edge(6, 8),
+        edge(7, 8)
+    );
+    assertThat(problem.getNumberOfBins()).isEqualTo(3);
+  }
+
+  private Edge edge(int v1, int v2) {
+    return new Edge(new Vertex(v1), new Vertex(v2));
+  }
 }
