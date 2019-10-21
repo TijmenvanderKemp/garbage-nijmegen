4d09256e51c61a14a5ce1dc136fc4fbc981df831
t.vanderkemp <t.vanderkemp@gouwit.nl>
2019-10-21 22:17:05 +0200

    Redundant method

--- src/test/java/com/tijmen/VertexLowerBoundTest.java
+++ src/test/java/com/tijmen/VertexLowerBoundTest.java
@@ -1,8 +1,6 @@
 package com.tijmen;
 
-import com.tijmen.entities.Edge;
 import com.tijmen.entities.Problem;
-import com.tijmen.entities.Vertex;
 import org.junit.Test;
 
 import java.io.InputStream;
@@ -29,9 +27,5 @@ public class VertexLowerBoundTest {
     Algorithm algorithm = new Algorithm(problem);
 
     assertThat(algorithm.solve()).isTrue();
-  }
-
-  private Edge edge(int v1, int v2) {
-    return new Edge(new Vertex(v1), new Vertex(v2));
   }
 }
